db0bd91da8716766a10a0393ef1dc537ceb360ce
Timothy Whitehead <twhiteheadzm@gmail.com>
2020-03-06 19:06:56 +0200

    AssetIndex, RegistryId typesizes

--- src/events.rs
+++ src/events.rs
@@ -112,13 +112,15 @@ impl<T: System + Balances + 'static> TryFrom<Metadata> for EventsDecoder<T> {
         // VoteThreshold enum index
         decoder.register_type_size::<u8>("VoteThreshold")?;
 
-        // identity
+        // identity / asset-registry
         decoder.register_type_size::<u32>("CatalogId")?;
         decoder.register_type_size::<[u8; 32]>("H256")?;
         decoder.register_type_size::<[u8; 32]>("Did")?;
         decoder.register_type_size::<i64>("i64")?;
         decoder.register_type_size::<u64>("Moment")?;
         decoder.register_type_size::<u64>("ClaimIndex")?;
+        decoder.register_type_size::<u32>("RegistryId")?;
+        decoder.register_type_size::<u64>("AssetIndex")?;
 
         Ok(decoder)
     }
